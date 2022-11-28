83b7674d2bff9efcc08c8aa69e77816fd0f8d120
andreamorandi <morandiandrearocco@gmail.com>
2022-11-28 20:02:20 +0100

    Aggiunto stile

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ if (!empty($strength)) {
     $_SESSION["numbers"] = $_GET["numbers"] ?? false;
     $_SESSION["symbols"] = $_GET["symbols"] ?? false;
     header("Location: ./password.php");
+} else if (isset($strength)) {
+    $alert = true;
 }
 ?>
 
@@ -19,37 +21,56 @@ if (!empty($strength)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- CUSTOM STYLE -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+    <header>
+        <h1 class="text-secondary text-center my-4">Strong Password Generator</h1>
+        <h2 class="text-white text-center my-4">Genera una password sicura</h2>
+    </header>
     <main>
-        <form action="index.php" method="GET">
-            <div>
-                <label for="strength">Lunghezza password</label>
-                <input type="text" id="strength" name="strength">
-            </div>
+        <div class="container">
+            <?php
+            if ($alert) {
+                echo '
+                        <div class="alert alert-info" role="alert">
+                            Nessun parametro valido inserito
+                        </div>
+                    ';
+            }
+            ?>
+            <form action="index.php" method="GET">
+                <div class="d-flex justify-content-between">
+                    <label for="strength" class="form-label">Lunghezza password:</label>
+                    <input type="text" id="strength" name="strength" class="form-control strength-input">
+                </div>
 
-            <div>
-                <input type="radio" id="rep_true" name="repetition" value=true checked>
-                <label for="rep_true">Consenti ripetizioni</label><br>
-                <input type="radio" id="rep_false" name="repetition" value=false>
-                <label for="rep_false">Non consentire ripetizioni</label><br>
-            </div>
+                <div class="d-flex justify-content-between mt-3">
+                    <p>Consenti ripetizioni di uno o più caratteri:</p>
+                    <div class="form-check password-features">
+                        <input type="radio" id="rep_true" name="repetition" value=true checked class="form-check-input">
+                        <label for="rep_true" class="form-label">Sì</label><br>
+                        <input type="radio" id="rep_false" name="repetition" value=false class="form-check-input">
+                        <label for="rep_false" class="form-label">No</label><br>
+                        <input type="checkbox" id="letters" name="letters" value="true" class="form-check-input">
+                        <label for="letters" class="form-check-label"> Lettere</label><br>
+                        <input type="checkbox" id="numbers" name="numbers" value="true" class="form-check-input">
+                        <label for="numbers" class="form-check-label"> Numeri</label><br>
+                        <input type="checkbox" id="symbols" name="symbols" value="true" class="form-check-input">
+                        <label for="symbols" class="form-check-label"> Simboli</label><br>
+                    </div>
+                </div>
 
-            <div>
-                <input type="checkbox" id="letters" name="letters" value="true">
-                <label for="letters"> Lettere</label><br>
-                <input type="checkbox" id="numbers" name="numbers" value="true">
-                <label for="numbers"> Numeri</label><br>
-                <input type="checkbox" id="symbols" name="symbols" value="true">
-                <label for="symbols"> Simboli</label><br>
-            </div>
-
-            <div>
-                <button type="submit">Genera</button>
-                <button type="reset">Annulla</button>
-            </div>
-        </form>
+                <div>
+                    <button type="submit" class="btn btn-primary">Genera</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                </div>
+            </form>
+        </div>
     </main>
 </body>
 
--- password.php
+++ password.php
@@ -2,15 +2,6 @@
 require_once __DIR__ . "/partials/functions.php";
 require_once __DIR__ . "/partials/data.php";
 session_start();
-// if (empty($_SESSION["letters"])) {
-//     $_SESSION["letters"] = false;
-// }
-// if (empty($_SESSION["numbers"])) {
-//     $_SESSION["numbers"] = false;
-// }
-// if (empty($_SESSION["symbols"])) {
-//     $_SESSION["symbols"] = false;
-// }
 $permitted_chars = set_permitted_chars($_SESSION["letters"], $_SESSION["numbers"], $_SESSION["symbols"],);
 $password = generate_password($_SESSION["strength"], $permitted_chars, $_SESSION["repetition"]);
 ?>
@@ -23,10 +14,16 @@ $password = generate_password($_SESSION["strength"], $permitted_chars, $_SESSION
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- CUSTOM STYLE -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <h2><?php echo $password ?></h2>
+    <div class="alert alert-info password-alert" role="alert">
+        <h2 class="text-black"><span class="mx-3">Password:</span> <?php echo $password ?></h2>
+    </div>
 </body>
 
 </html>
